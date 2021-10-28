a523a44216169e964afd55b6df52eb9e4f21eeea
Sloan <Sloan.boyce@gmail.com>
2021-10-27 23:06:46 -0400

    attempted bug with the jqueryUI sortable function

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -31,7 +31,6 @@ var loadTasks = function() {
     };
   }
 
-  // loop over object properties
   $.each(tasks, function(list, arr) {
     console.log(list, arr);
     // then loop over sub-array
@@ -43,6 +42,7 @@ var loadTasks = function() {
 
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+  console.log("Task");
 };
 
 // catches click of button 
@@ -94,7 +94,7 @@ $(".list-group").on("blur", "input[type='text']", function() {
       .closest(".list-group-item")
       .index();
     
-    tasks[status][index] = date;
+    tasks[status][index].date = date;
     saveTasks();
 
     var taskSpan = $("<span>")
@@ -104,6 +104,7 @@ $(".list-group").on("blur", "input[type='text']", function() {
     
 
     $(this).replaceWith(taskSpan);
+
 });
 
 // catches when the user clicks off the button 
@@ -123,7 +124,7 @@ $(".list-group").on("blur", "textarea", function() {
     .closest(".list-group-item")
     .index();
 
-  tasks[status][index] = text;
+  tasks[status][index].text = text;
   saveTasks();
 
   var taskP = $("<p>")
@@ -131,6 +132,7 @@ $(".list-group").on("blur", "textarea", function() {
     .text(text);
   
   $(this).replaceWith(taskP);
+
 });
 
 
@@ -168,6 +170,72 @@ $("#task-form-modal .btn-primary").click(function() {
     saveTasks();
   }
 });
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+    
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+    
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+    console.log(arrName);
+    console.log($(this).children())
+  }
+
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+})
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
